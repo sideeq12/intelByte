fa4f876474b29985dcbf67e5c101d87404595e9f
sideeq12 <abdwaheed2018@gmail.com>
2022-01-29 17:41:40 +0000

    nav

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import { Blue } from './components/bluelayer/bluelayer';
 import { Flash } from './components/flash-sales/flash-sales';
+import { NavBar } from './components/navbar/navbar';
 import { Products } from './components/products/product';
 import { Recommended } from './components/recommended/recommended';
 import { Welcome } from './components/welcome/welcome';
@@ -9,11 +10,13 @@ import { Welcome } from './components/welcome/welcome';
 
 const App = () =>{
   return (<div className='App'>
+    <NavBar />
     <Welcome />
     <Flash />
     <Products />
     <Recommended />
     <Blue />
+    
     </div>
   );
 }
---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import "./navbar.css"
+
+export const NavBar = ()=>{
+    return(
+        <div className='navBar'>
+            <nav>IntelByte</nav>
+            <div className='search'>
+                <input type="text" />
+                <button type="submit">Search</button>
+            </div>
+            <div className='menu'>
+                <div className='nav_link'>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+  <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
+</svg>
+                    <label> Account </label>
+                </div>
+                <div className='nav_link'>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart-plus" viewBox="0 0 16 16">
+  <path d="M9 5.5a.5.5 0 0 0-1 0V7H6.5a.5.5 0 0 0 0 1H8v1.5a.5.5 0 0 0 1 0V8h1.5a.5.5 0 0 0 0-1H9V5.5z"/>
+  <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zm3.915 10L3.102 4h10.796l-1.313 7h-8.17zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+</svg>
+                    <label> Cart </label>
+                </div>
+            </div>
+        </div>
+    )
+}
